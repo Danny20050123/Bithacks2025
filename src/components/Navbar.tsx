b5f70a9650b07d1289ef0e5e72574b44625b0679
Julia Nguyen <juliatnguyen05@gmail.com>
2025-02-04 19:35:32 -0800

    add basic navbar layout

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,9 +1,20 @@
 import React from "react";
+import Image from "next/image";
 
 const Navbar = () => {
   return (
-    
-    <p>Navbar+intro</p>
+    <>
+      <div className="flex justify-between items-center px-10 py-6">
+        <Image src="globe.svg" width={36} height={36} alt={"BitHacks Logo"} />
+        <div className="flex gap-8 justify-end w-fit">
+          <p>About</p>
+          <p>Tracks</p>
+          <p>Faq</p>
+          <p>Sponsors</p>
+          <p>Team</p>
+        </div>
+      </div>
+    </>
   );
 };
 
