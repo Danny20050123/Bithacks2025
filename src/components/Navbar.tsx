92dee0f558ba366a50291ec6c054097bd617fd2c
Julia Nguyen <juliatnguyen05@gmail.com>
2025-02-06 19:43:13 -0800

    center hero text

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -7,11 +7,24 @@ const Navbar = () => {
       <div className="flex justify-between items-center px-10 py-6">
         <Image src="globe.svg" width={36} height={36} alt={"BitHacks Logo"} />
         <div className="flex gap-8 justify-end w-fit">
-          <p>About</p>
-          <p>Tracks</p>
-          <p>Faq</p>
-          <p>Sponsors</p>
-          <p>Team</p>
+          <a>About</a>
+          <a>Tracks</a>
+          <a>Faq</a>
+          <a>Sponsors</a>
+          <a>Team</a>
+        </div>
+      </div>
+
+      <div className="h-[75vh] flex justify-center flex-col pl-10">
+        <div className="pb-4">
+          <p className="text-8xl">BitHacks</p>
+          <p className="text-xl pl-1">UCI's first embedded hackathon</p>
+        </div>
+        <div className="gap-3.5 flex">
+          <button className="bg-violet-900 px-2 py-1.5 w-fit">Hacker</button>
+          <button className="border-violet-900 border-2 border-solid px-2 py-1.5 w-fit">
+            Mentor
+          </button>
         </div>
       </div>
     </>
