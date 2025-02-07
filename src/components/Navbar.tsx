8964f6230fcfd23627e77d9be4c3b6ea8b7976ce
Julia Nguyen <juliatnguyen05@gmail.com>
2025-02-06 21:03:18 -0800

    add event details section

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -27,6 +27,22 @@ const Navbar = () => {
           </button>
         </div>
       </div>
+
+      {/* <div className="h-[75vh] bg-violet-900 flex items-center">
+        <p className="text-5xl text-right pl-[46rem] pr-[10rem]">
+          BitHacks is the when you biggest and new. This is the all time
+          electrical and embedded hackathon. Be there or be square.
+        </p>
+      </div> */}
+
+      <div className="pl-[10rem] h-[75vh] flex flex-col justify-center">
+        <div>
+          <p>Date | Location</p>
+          <p className="text-5xl">36 Hours</p>
+          <p className="text-5xl pb-4">$60,000+ Prizes</p>
+        </div>
+        <button className="bg-violet-900 px-2 py-1.5 w-fit">Sponsor</button>
+      </div>
     </>
   );
 };
