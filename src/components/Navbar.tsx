06db30cf97fec16b9e78a4d276f572fbb5d9ef4c
Julia Nguyen <juliatnguyen05@gmail.com>
2025-02-11 14:41:12 -0800

    fix non responsive about text

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -7,27 +7,29 @@ import { motion } from "framer-motion";
 const Navbar = () => {
   return (
     <>
-      <nav id="home" className="flex justify-between items-center px-10 py-6">
-        <motion.div whileHover={{ scale: 1.2 }}>
-          <a href="#home">
-            <Image
-              src="globe.svg"
-              width={36}
-              height={36}
-              alt={"BitHacks Logo"}
-            />
-          </a>
-        </motion.div>
-        <div className="flex gap-8 justify-end w-fit">
-          <a href="#about">About</a>
-          <a href="#tracks">Tracks</a>
-          <a href="#faq">FAQ</a>
-          <a href="#sponsors">Sponsors</a>
-          <a href="#team">Team</a>
-        </div>
-      </nav>
+      <div id="home">
+        <nav className="flex justify-between items-center px-10 py-6">
+          <motion.div whileHover={{ scale: 1.2 }}>
+            <a href="#home">
+              <Image
+                src="globe.svg"
+                width={36}
+                height={36}
+                alt={"BitHacks Logo"}
+              />
+            </a>
+          </motion.div>
+          <div className="flex gap-8 justify-end w-fit">
+            <a href="#about">About</a>
+            <a href="#tracks">Tracks</a>
+            <a href="#faq">FAQ</a>
+            <a href="#sponsors">Sponsors</a>
+            <a href="#team">Team</a>
+          </div>
+        </nav>
+      </div>
 
-      <div className="h-[90vh] flex justify-center flex-col pl-[10rem]">
+      <div className="h-[90vh] w-fit flex justify-center flex-col pl-[10rem]">
         <div className="pb-4">
           <p className="text-8xl">BitHacks</p>
           <p className="text-xl pl-1">UCI's first embedded hackathon</p>
@@ -54,7 +56,7 @@ const Navbar = () => {
 
       <div className="relative">
         <div id="about" className="h-[65vh] bg-violet-500 flex items-center">
-          <p className="text-5xl text-right pr-[10rem] pl-[40rem] py-[4rem]">
+          <p className="text-[3vw] text-right pl-[48rem] right-[10rem] leading-[3vw] absolute">
             BitHacks is the when you biggest and new. This is the all time
             electrical and embedded hackathon. Be there or be square.
           </p>
@@ -68,13 +70,18 @@ const Navbar = () => {
         ></div>
       </div>
 
-      <div className="pl-[10rem] h-[60vh] flex flex-col justify-center">
+      <div className="pl-[10rem] h-[60vh] w-fit flex flex-col justify-center">
         <div>
           <p className="">Date | Location</p>
           <p className="text-5xl">36 Hours</p>
           <p className="text-5xl pb-4">$60,000+ Prizes</p>
         </div>
-        <button className="bg-violet-500 px-4 py-1.5 w-fit">Sponsor</button>
+        <motion.button
+          whileHover={{ scale: 1.2, backgroundColor: "#724CF1" }}
+          className="bg-violet-500 px-4 py-1.5 w-fit"
+        >
+          Sponsor
+        </motion.button>
       </div>
     </>
   );
