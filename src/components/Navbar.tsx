ab6db2eb3f5de996d4e632f89761dd2f36e735e4
Julia Nguyen <juliatnguyen05@gmail.com>
2025-02-16 12:39:13 -0800

    add mobile dropdown animation

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,11 +1,16 @@
 "use client";
 
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Image from "next/image";
-import { motion } from "framer-motion";
+import { motion, cubicBezier, AnimatePresence } from "framer-motion";
 
 const Navbar = () => {
   const [navbarOpen, setNavbarOpen] = useState(false);
+
+  let bezier;
+  useEffect(() => {
+    bezier = cubicBezier(0.05, 0.8, 0.45, 0.95);
+  });
 
   return (
     <>
@@ -14,14 +19,17 @@ const Navbar = () => {
           {/* <nav className="flex justify-between items-center px-10 py-6"> */}
           {/* <motion.div whileHover={{ scale: 1.2 }}> */}
           <a href="#home">
-            <Image
-              src="globe.svg"
-              width={36}
-              height={36}
-              alt={"bithacks logo"}
-            />
+            <motion.div whileTap={{ scale: 0.9 }}>
+              <Image
+                src="globe.svg"
+                width={36}
+                height={36}
+                alt={"bithacks logo"}
+              />
+            </motion.div>
           </a>
           <motion.div
+            whileTap={{ scale: 0.9 }}
             onClick={() => {
               setNavbarOpen(!navbarOpen);
             }}
@@ -43,16 +51,30 @@ const Navbar = () => {
             <a href="#team">Team</a>
           </div> */}
         </nav>
-        <div className="absolute right-10">
-          {navbarOpen && (
-            <ul className="bg-purple-700 flex flex-col text-right px-8 py-4 w-fit gap-8">
-              <a href="#about">About</a>
-              <a href="#tracks">Tracks</a>
-              <a href="#faq">FAQ</a>
-              <a href="#sponsors">Sponsors</a>
-              <a href="#team">Team</a>
-            </ul>
-          )}
+        <div>
+          <AnimatePresence>
+            {navbarOpen && (
+              <motion.div
+                initial={{ opacity: 0, y: 0 }}
+                animate={{ opacity: 1, y: 20 }}
+                exit={{ opacity: 0, y: 0 }}
+                transition={{
+                  duration: 0.25,
+                  ease: bezier,
+                  type: "spring",
+                }}
+                className="absolute right-10"
+              >
+                <ul className="bg-purple-700 flex flex-col text-right px-8 py-4 w-fit gap-8">
+                  <a href="#about">About</a>
+                  <a href="#tracks">Tracks</a>
+                  <a href="#faq">FAQ</a>
+                  <a href="#sponsors">Sponsors</a>
+                  <a href="#team">Team</a>
+                </ul>
+              </motion.div>
+            )}
+          </AnimatePresence>
         </div>
       </div>
     </>
