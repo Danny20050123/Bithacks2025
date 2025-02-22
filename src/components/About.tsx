b1da0e4f9219e465a56008b3efce85ebe80f841c
Julia Nguyen <juliatnguyen05@gmail.com>
2025-02-22 15:57:33 -0800

    refactor responsiveness for mobile and desktop about section

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -12,7 +12,7 @@ const About = () => {
         > */}
         <div id="about" className="h-[65vh] bg-violet-500 flex items-center">
           {/* <p className="md:text-5xl text-[2vw] text-right pl-[50vw] right-[10rem] lg:leading-[48px] md:leading-[3vw] leading-[2vw] absolute"> */}
-          <p className="md:text-5xl text-[1.5rem] text-right px-10 md:pl-[50vw] md:right-[10rem] lg:leading-[48px] md:leading-[3vw] absolute">
+          <p className="text-[1.5rem] md:text-4xl md:leading-[3rem] md:pr-10 lg:pl-[48vw] md:pl-[20vw] pt-8 text-right max-md:px-10 absolute">
             As UCI's first embedded systems hackathon, BitHacks is focused on
             providing a space for students to <b>create</b>, <b>explore</b>, and{" "}
             <b>grow</b>. BitHacks aims to foster creativity and{" "}
@@ -31,8 +31,8 @@ const About = () => {
       <div className="pl-10 md:pl-[10rem] h-[60vh] w-fit flex flex-col justify-center">
         <div>
           <p className="">April 4-6 • Location</p>
-          <p className="text-5xl">36 Hours</p>
-          <p className="text-5xl pb-4">$500+ Prizes</p>
+          <p className="text-4xl md:text-5xl">36 Hours</p>
+          <p className="text-4xl md:text-5xl pb-4">$500+ Prizes</p>
         </div>
         <motion.button
           whileHover={{ scale: 1.2, backgroundColor: "#724CF1" }}
