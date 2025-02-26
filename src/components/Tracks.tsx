cd740eea1cdbc820dc254d90fe7697d9739c1f43
Daniel Li <ningyl1@uci.edu>
2025-02-25 18:08:32 -0800

    Added navbar functionality

--- src/components/Tracks.tsx
+++ src/components/Tracks.tsx
@@ -5,7 +5,7 @@ import CardComponent from "./Card";
 
 const Tracks = () => {
   return (
-    <div className="flex justify-center bg-[#43345d] min-h-[800px] rounded-[15px]">
+    <div id = "tracks" className="flex justify-center bg-[#43345d] min-h-[800px] rounded-[15px]">
       {/* <p>Tracks area</p> */}
       <div className="grid gap-5 p-5">
         <CardComponent title="hi" content="there" />
