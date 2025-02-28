8b26e48cd6fd46f9f526f382546b8fd98080dddf
Dhivyesh Kanagasabai <dkanagas@uci.edu>
2025-02-28 00:17:34 -0800

    fixed alignment issue

--- src/components/Tracks.tsx
+++ src/components/Tracks.tsx
@@ -8,7 +8,7 @@ const Tracks = () => {
     <div id = "tracks" className="flex justify-center bg-[#43345d] min-h-[800px] rounded-[15px]">
       {/* <p>Tracks area</p> */}
       <div className="flex flex-col gap-5 p-5">
-        <div className="flex flex-col gap-20 p-5 md:flex-row gap-20">
+        <div className="flex flex-col gap-5 p-5 md:flex-row gap-20">
         <CardComponent title="hi" content="there" />
         <CardComponent title="hello" content="world" />
         <CardComponent title="hi" content="there" />
