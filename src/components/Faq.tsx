56fc0c8d4c494895dcf26311f595ed2b879e0caa
Julia Nguyen <juliatnguyen05@gmail.com>
2025-03-05 17:32:38 -0800

    fix horizontal scroll faq

--- src/components/Faq.tsx
+++ src/components/Faq.tsx
@@ -13,11 +13,13 @@ const Faq = () => {
   const faqData = [
     {
       question: "What is BitHacks?",
-      answer: "BitHacks is an event by IEEE and Theta Tau where hackers collaborate and compete to create the most innovative embedded projects within a weekend."
+      answer:
+        "BitHacks is an event by IEEE and Theta Tau where hackers collaborate and compete to create the most innovative embedded projects within a weekend.",
     },
     {
       question: "Who can participate in BitHacks?",
-      answer: "Any college student can apply to participate in BitHacks. We welcome hackers of all skill levels, no prior coding or embedded experience is needed."
+      answer:
+        "Any college student can apply to participate in BitHacks. We welcome hackers of all skill levels, no prior coding or embedded experience is needed.",
     },
     // {
     //   question: "Do I need to know how to code or work with embedded software?",
@@ -25,7 +27,8 @@ const Faq = () => {
     // },
     {
       question: "When do applications close?",
-      answer: "Applications close March 21. Decision details will be released closer to the event."
+      answer:
+        "Applications close March 21. Decision details will be released closer to the event.",
     },
     // {
     //   question: "What should I bring?",
@@ -33,51 +36,96 @@ const Faq = () => {
     // },
     {
       question: "What parts will be provided to use?",
-      answer: <>Here is a <a href="https://docs.google.com/spreadsheets/d/e/2PACX-1vQUZ75eky0jHpDp_vapfN2ATBrxrkZ5mY5WN1bJKSkdKcVwsXL1uxg0MRQaQDway5Vtx2UjyzXWsiAt/pubhtml" className="text-blue-600 hover:text-blue-800 underline" target="_blank" rel="noopener noreferrer">list</a> for all the parts that will be provided!</>
+      answer: (
+        <>
+          Here is a{" "}
+          <a
+            href="https://docs.google.com/spreadsheets/d/e/2PACX-1vQUZ75eky0jHpDp_vapfN2ATBrxrkZ5mY5WN1bJKSkdKcVwsXL1uxg0MRQaQDway5Vtx2UjyzXWsiAt/pubhtml"
+            className="text-blue-600 hover:text-blue-800 underline"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            list
+          </a>{" "}
+          for all the parts that will be provided!
+        </>
+      ),
     },
     {
       question: "Can I bring my own parts to use?",
-      answer: "Feel free to bring your own parts as long as they are under $50."
+      answer:
+        "Feel free to bring your own parts as long as they are under $50.",
     },
     {
       question: "What is the schedule like?",
-      answer: "Check-in ends at 7 PM on April 11 and hacking will conclude on Sunday, April 13 at 9 AM."
-    }, 
+      answer:
+        "Check-in ends at 7 PM on April 11 and hacking will conclude on Sunday, April 13 at 9 AM.",
+    },
     {
       question: "Will transportation be provided?",
-      answer: "No, transportation will need to be taken care of by individual hackers. Unfortunately we can't provide any travel reimbursements."
+      answer:
+        "No, transportation will need to be taken care of by individual hackers. Unfortunately we can't provide any travel reimbursements.",
     },
     {
       question: "What if I don't have a team?",
-      answer: "There will be a team-building event before the start of BitHacks!"
-    }
+      answer:
+        "There will be a team-building event before the start of BitHacks!",
+    },
   ];
 
   const sponsorData = [
-    { name: "IEEE-Eta Kappa Nu", link: "https://hkn.ieee.org/", imageUrl: "/IEEE-EKN.png" },
-    { name: "Medtronic", link: "https://www.medtronic.com/", imageUrl: "/medtronic-3.svg" },
-    { name: "Northrop Grumman", link: "https://www.northropgrumman.com/", imageUrl: "/NorthropGrumman.svg" },
-    { name: "UCI MECPS", link: "https://mecps.uci.edu/", imageUrl: "/MECPS.png" },
-    { name: "UCI Samueli School of Engineering", link: "https://engineering.uci.edu/", imageUrl: "/samueli.png" },
-    { name: "UCI ODIT", link: "https://odit.uci.edu", imageUrl: "/ODITLogoTransparent.png"},
-    { name: "UCI OAI", link: "https://oai.tech.uci.edu", imageUrl: "/inclusion-oai.png" }
+    {
+      name: "IEEE-Eta Kappa Nu",
+      link: "https://hkn.ieee.org/",
+      imageUrl: "/IEEE-EKN.png",
+    },
+    {
+      name: "Medtronic",
+      link: "https://www.medtronic.com/",
+      imageUrl: "/medtronic-3.svg",
+    },
+    {
+      name: "Northrop Grumman",
+      link: "https://www.northropgrumman.com/",
+      imageUrl: "/NorthropGrumman.svg",
+    },
+    {
+      name: "UCI MECPS",
+      link: "https://mecps.uci.edu/",
+      imageUrl: "/MECPS.png",
+    },
+    {
+      name: "UCI Samueli School of Engineering",
+      link: "https://engineering.uci.edu/",
+      imageUrl: "/samueli.png",
+    },
+    {
+      name: "UCI ODIT",
+      link: "https://odit.uci.edu",
+      imageUrl: "/ODITLogoTransparent.png",
+    },
+    {
+      name: "UCI OAI",
+      link: "https://oai.tech.uci.edu",
+      imageUrl: "/inclusion-oai.png",
+    },
     // { name: "Test8", link: "https://test8.com", imageUrl: "/api/placeholder/150/150" }
   ];
 
   const toggleFaq = (index: number) => {
-    setOpenFaqs(prev => 
-      prev.includes(index) 
-        ? prev.filter(i => i !== index)
-        : [...prev, index]
+    setOpenFaqs((prev) =>
+      prev.includes(index) ? prev.filter((i) => i !== index) : [...prev, index]
     );
   };
 
   return (
-    <div id = "faq" className="w-full relative">
+    <div id="faq" className="w-full relative">
       {/* FAQs Section */}
       <div className="bg-[#6C81E6] px-6 py-12 relative">
-        <h2 className="font-jersey text-5xl mb-8 text-left text-[#332FAC]">FAQ</h2>
-        
+        <h2 className="font-jersey text-5xl mb-8 text-left text-[#332FAC]">
+          FAQ
+        </h2>
+
         <div className="grid md:grid-cols-2 gap-4 mb-32">
           {faqData.map((faq, index) => (
             <div key={index} className="flex flex-col">
@@ -85,23 +133,26 @@ const Faq = () => {
                 onClick={() => toggleFaq(index)}
                 className={`bg-[#A6EFFF] p-4 flex justify-between items-center w-full text-[#332FAC]
                   transition-all duration-300 ease-in-out
-                  ${openFaqs.includes(index) 
-                    ? 'rounded-t-lg' // top corners rounded when open
-                    : 'rounded-lg'   // All corners rounded when closed
+                  ${
+                    openFaqs.includes(index)
+                      ? "rounded-t-lg" // top corners rounded when open
+                      : "rounded-lg" // All corners rounded when closed
                   }`}
               >
-                <span className="font-rethink text-left pr-4 whitespace-normal">{faq.question}</span>
+                <span className="font-rethink text-left pr-4 whitespace-normal">
+                  {faq.question}
+                </span>
                 <ChevronDown
                   className={`transform transition-transform duration-200 text-black flex-shrink-0 ${
-                    openFaqs.includes(index) ? 'rotate-180' : ''
+                    openFaqs.includes(index) ? "rotate-180" : ""
                   }`}
                 />
               </button>
-              <div 
+              <div
                 className={`grid transition-all duration-300 ease-in-out ${
-                  openFaqs.includes(index) 
-                    ? 'grid-rows-[1fr] opacity-100' 
-                    : 'grid-rows-[0fr] opacity-0'
+                  openFaqs.includes(index)
+                    ? "grid-rows-[1fr] opacity-100"
+                    : "grid-rows-[0fr] opacity-0"
                 }`}
               >
                 <div className="overflow-hidden">
@@ -112,63 +163,64 @@ const Faq = () => {
               </div>
             </div>
           ))}
-
         </div>
 
         <div className="absolute bottom-10 left-4 w-[6rem] h-[6rem] z-[5]">
-        <img 
-          src="/star_pixel_blue.svg" 
-          alt="Blue Star" 
-          className="w-full h-full object-contain"
-        />
-      </div> 
-      
+          <img
+            src="/star_pixel_blue.svg"
+            alt="Blue Star"
+            className="w-full h-full object-contain"
+          />
+        </div>
+
         {/* Diagonal Divider */}
-        <div 
+        <div
           className="absolute left-0 right-0 w-full "
           style={{
-            height: '100px',
-            background: 'linear-gradient(to right bottom, #6C81E6 49.5%, #DFBAFF 50%)',
-            marginTop: '-52px'
+            height: "100px",
+            background:
+              "linear-gradient(to right bottom, #6C81E6 49.5%, #DFBAFF 50%)",
+            marginTop: "-52px",
           }}
         />
       </div>
 
       {/* Sponsors Section */}
-      <div id = "sponsors" className="bg-[#DFBAFF] px-6 py-16">
-        
+      <div id="sponsors" className="bg-[#DFBAFF] px-6 py-16">
         {/* Mobile Anteater Graphic */}
         <div className="sm:hidden w-[40rem] h-[40rem] -translate-x-[1rem] -mt-20 overflow-hidden">
-          <img 
-            src="/kick_character.PNG" 
-            alt="Anteater kicking" 
+          <img
+            src="/kick_character.PNG"
+            alt="Anteater kicking"
             className="w-full h-full object-contain"
           />
         </div>
 
-        <h2 className="font-jersey text-5xl mb-12 text-center text-[#332FAC]">Sponsors</h2>
-        
+        <h2 className="font-jersey text-5xl mb-12 text-center text-[#332FAC]">
+          Sponsors
+        </h2>
+
         {/* Sponsors Pink Star */}
         <div className="absolute bottom-21 right-0 w-[7rem] h-[7rem] mb-20 mr-6 hidden lg:block overflow-hidden">
-          <img 
+          <img
             src="/star_pixel_light_pink.svg"
-            alt="Pink Star" 
+            alt="Pink Star"
             className="w-full h-full object-contain"
           />
         </div>
 
         {/* Anteater Kick Graphic */}
-        <div className="absolute -bottom-20 -right-5 w-[50rem] h-[40rem] -mb-20 mr-0 hidden lg:block overflow-hidden">
-          <img 
-            src="/kick_character.PNG" 
-            alt="Anteater kicking" 
+        <div className="absolute -bottom-20 -right-0 w-[40rem] h-[30rem] -mb-20 mr-0 hidden lg:block overflow-hidden">
+          <img
+            src="/kick_character.PNG"
+            alt="Anteater kicking"
             className="w-full h-full object-contain translate-x-1/4"
           />
         </div>
 
         <div className="grid grid-cols-2 md:grid-cols-4 gap-6 max-w-5xl mx-auto">
           {sponsorData.map((sponsor, index) => (
-            <div 
+            <div
               key={index}
               className="aspect-square bg-white rounded-lg shadow-md p-4 relative group overflow-hidden"
             >
@@ -180,8 +232,10 @@ const Faq = () => {
               {/* Hover overlay w/animation */}
               <div className="absolute inset-0 bg-black bg-opacity-50 flex items-center justify-center opacity-0 group-hover:opacity-100 transition-opacity duration-300">
                 <div className="text-center transform translate-y-4 group-hover:translate-y-0 transition-transform duration-300">
-                  <p className="text-white text-xl font-semibold mb-2">{sponsor.name}</p>
-                  <a 
+                  <p className="text-white text-xl font-semibold mb-2">
+                    {sponsor.name}
+                  </p>
+                  <a
                     href={sponsor.link}
                     className="text-white text-l underline hover:text-blue-200 transition-colors"
                     target="_blank"
@@ -194,11 +248,11 @@ const Faq = () => {
             </div>
           ))}
         </div>
-          {/* Mobile Star Graphic */}
-          <div className="sm:hidden bottom-0 mt-12 w-[7rem] h-[7rem] translate-x-[14.75rem] overflow-hidden">
-          <img 
+        {/* Mobile Star Graphic */}
+        <div className="sm:hidden bottom-0 mt-12 w-[7rem] h-[7rem] translate-x-[14.75rem] overflow-hidden">
+          <img
             src="/star_pixel_light_pink.svg"
-            alt="Light Pink Star" 
+            alt="Light Pink Star"
             className="w-full h-full object-contain"
           />
         </div>
