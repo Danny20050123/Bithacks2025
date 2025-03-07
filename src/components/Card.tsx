71abda31249fb5ffca13e5c5fa8f09ca6bd6d027
Dhivyesh Kanagasabai <dkanagas@uci.edu>
2025-03-06 21:18:11 -0800

    changed bg color of track images

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -10,7 +10,7 @@ const CardComponent: React.FC<CardProps> = ({ title, content, img}) => {
   return (
     <div className="flex row items-center justify-center rounded-[15px]">
       <div className="flex max-w-[100vw] h-[215px] bg-white flex flex-col shadow-[0_5px_20px_rgba(0,0,0,0.5)] transition-[0.3s] duration-[ease-in-out] mx-2.5 my-[30px] px-[15px] py-5 rounded-[15px] hover:h-80">
-        <div className="relative items-center justify-center w-[260px] h-[260px] top-[-100%] shadow-[0_5px_20px_rgba(0,0,0,0.2)] z-[1] rounded-[15px] perspective-distant transition-transform duration-300 transform hover: visible opacity-100 scale-105">
+        <div className="relative items-center justify-center bg-[#B69BFB] w-[260px] h-[260px] top-[-100%] shadow-[0_5px_20px_rgba(0,0,0,0.2)] z-[1] rounded-[15px] perspective-distant transition-transform duration-300 transform hover: visible opacity-100 scale-105">
           <img
             className="max-w-full rounded-[15px] bg-[#] h-[220px] w-[260px] object-cover"
             src={img}
