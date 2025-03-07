062964832b64e3a54079c5f032ac6cec41b3fdb7
Dhivyesh Kanagasabai <dkanagas@uci.edu>
2025-03-06 21:32:53 -0800

    changed font for tracks

--- src/components/Tracks.tsx
+++ src/components/Tracks.tsx
@@ -20,7 +20,7 @@ const Tracks = () => {
       <img className="md:absolute h-[400px] w-[600px] left-0 justify-left items-left [transform:rotateY(180deg)]" src="https://s3-alpha-sig.figma.com/img/ca70/c85b/5eb3587c8a062897708a9158999634a9?Expires=1742169600&Key-Pair-Id=APKAQ4GOSFWCW27IBOMQ&Signature=Y0cK16tfDh-KuhEZVI7u-iUdwhC0FfpD2Dve0h4ZgW3c1k~tUhADeOXO7RwJGKC~DeHOrnjHFbaXY-c6sUCZMhIaeaEOdJd6IIm9hQRFtAHDnkDfXVh-rxdrQLckLWqq7GutQdoxJuAm7fROLMOuMPIdQw6Z4v5hp5-ABdqlT6R5zT1snWpAPFPG8qoUgXsKvTSxTJEfc-2wAyz5ajOZceiLzAEymRV01x2VkS3vEZN1WbQoq5b2MZgE6N1jOUJhHTtGBL2HRYryz2Z3v6AYwKsO9GYTle0FeJf4Fpe8gEA5tZHbHAsbRNyX3j~xbWWJfOu5MIBwOq939Rf9aACeqw__" alt="Decorative content"/>
       </div>
       <div className="flex flex-col gap-5 p-5 text-center justify-center">
-      <h1 className="relative flex text-center justify-center h-[100px] text-4xl top-[-50px]">Tracks</h1>
+      <h1 className="relative flex text-center justify-center font-jersey h-[100px] text-4xl top-[-50px]">Tracks</h1>
         <div className="relative flex flex-col top-[0px] gap-20 p-5 md:flex-row gap-20">
         <CardComponent title="Best Overall 🏆" content="Awarded to the most outstanding hack that excels in creativity, functionality, and impact." img="/best_overall.png" />
         <CardComponent title="Best Beginner 🚀" content="A special prize for first-time hackers who impress with their innovation and effort." img="/best_beginner.png" />
