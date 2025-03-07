6e1a6d2f3505c7ee6dc5e8c9215aa749b5a7764d
Dhivyesh Kanagasabai <dkanagas@uci.edu>
2025-03-06 20:52:35 -0800

    finished tracks for desktop view

--- src/components/Tracks.tsx
+++ src/components/Tracks.tsx
@@ -9,8 +9,8 @@ import CardComponent from "./Card";
 
 const Tracks = () => {
   return (
-    <div className="flex justify-center bg-[#B69BFB] min-h-[800px] rounded-[15px] md:h-[1000px]" id = "tracks">
-      <div className="invisble md:absolute opacity-100 top-[2050px] h-[400px] w-[600px] left-10 justify-left items-left invisible md:visible absolute opactiy-100 top-[2600px] h-[400px] w-[600px] left-0 justify-left items-left">
+    <div className="flex justify-center bg-[#B69BFB] h-[2400px] rounded-[15px] md:h-[1000px]" id = "tracks">
+      <div className="invisble md:absolute opacity-100 top-[2000px] h-[400px] w-[600px] left-10 justify-left items-left invisible md:visible absolute opactiy-100 top-[2600px] h-[400px] w-[600px] left-0 justify-left items-left">
       <img className="md:absolute h-[200px] w-[200px] left-0 justify-left items-left rotate-12" src="/star_pixel_pink.svg" alt="Decorative content"/>
       </div>
       <div className="invisible md:absolute opacity-100 top-[2600px] h-[400px] w-[600px] right-10 justify-right items-right invisible md:visible absolute opactiy-100 top-[2600px] h-[800px] w-[600px] right-0 justify-right items-right">
@@ -20,17 +20,16 @@ const Tracks = () => {
       <img className="md:absolute h-[400px] w-[600px] left-0 justify-left items-left [transform:rotateY(180deg)]" src="https://s3-alpha-sig.figma.com/img/ca70/c85b/5eb3587c8a062897708a9158999634a9?Expires=1742169600&Key-Pair-Id=APKAQ4GOSFWCW27IBOMQ&Signature=Y0cK16tfDh-KuhEZVI7u-iUdwhC0FfpD2Dve0h4ZgW3c1k~tUhADeOXO7RwJGKC~DeHOrnjHFbaXY-c6sUCZMhIaeaEOdJd6IIm9hQRFtAHDnkDfXVh-rxdrQLckLWqq7GutQdoxJuAm7fROLMOuMPIdQw6Z4v5hp5-ABdqlT6R5zT1snWpAPFPG8qoUgXsKvTSxTJEfc-2wAyz5ajOZceiLzAEymRV01x2VkS3vEZN1WbQoq5b2MZgE6N1jOUJhHTtGBL2HRYryz2Z3v6AYwKsO9GYTle0FeJf4Fpe8gEA5tZHbHAsbRNyX3j~xbWWJfOu5MIBwOq939Rf9aACeqw__" alt="Decorative content"/>
       </div>
       <div className="flex flex-col gap-5 p-5 text-center justify-center">
-      <h1 className="flex text-center justify-center h-[100px] text-4xl">Tracks</h1>
-        <div className="flex flex-col gap-5 p-5 md:flex-row gap-20">
-        <CardComponent title="hi" content="there" />
-        <CardComponent title="hello" content="world" />
-        <CardComponent title="hi" content="there" />
+      <h1 className="relative flex text-center justify-center h-[100px] text-4xl top-[-50px]">Tracks</h1>
+        <div className="relative flex flex-col top-[0px] gap-20 p-5 md:flex-row gap-20">
+        <CardComponent title="Best Overall 🏆" content="Awarded to the most outstanding hack that excels in creativity, functionality, and impact." img="/best_overall.png" />
+        <CardComponent title="Best Beginner 🚀" content="A special prize for first-time hackers who impress with their innovation and effort." img="/best_beginner.png" />
+        <CardComponent title="Hacker’s Choice 🗳️" content="Voted by fellow participants, this hack wins the community’s seal of approval." img="/hackers_choice.png" />
         </div>
 
-        <div className="flex flex-col gap-5 p-5 md:flex-row gap-20">
-        <CardComponent title="hi" content="there" />
-        <CardComponent title="hi" content="there" />
-        <CardComponent title="hi" content="there" />
+        <div className="relative flex flex-col justify-center top-[100px] gap-20 p-5 md:flex-row gap-20">
+        <CardComponent title="Medtronic Track 💙" content="Sponsored by Medtronic, this prize goes to the best healthcare or medical innovation." img="/medtronic.png" />
+        <CardComponent title="Meme Hack 😂" content="The funniest, most creative, and internet-worthy hack takes home this prize." img="/meme_hack.png" />
         </div>
       </div>
     </div>
