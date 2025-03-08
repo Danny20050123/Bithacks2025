7312c2f7231876682fbacc053804e40064a5c2d8
Julia Nguyen <juliatnguyen05@gmail.com>
2025-03-07 18:12:06 -0800

    add about graphic desktop

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,8 +1,9 @@
 "use client";
 
 import { useEffect } from "react";
-import { motion } from "motion/react";
+import AboutCard from "../components/AboutCard";
 import TypeIt from "typeit";
+import Image from "next/image";
 
 const About = () => {
   useEffect(() => {
@@ -40,38 +41,52 @@ const About = () => {
 
   return (
     <>
-      <div className="relative">
-        <div className="h-[65vh] md:h-[68vh] bg-[#DFBAFF] flex items-center">
-          <p className="font-rethink text-[1.5rem] md:text-4xl md:leading-[3rem] md:pr-[10rem] lg:pl-[44vw] md:pl-[20vw] pt-8 text-right max-md:px-10 absolute">
-            As UCI's first Embedded Systems hackathon, BitHacks is focused on
-            providing a space for students to <b>create</b>, <b>explore</b>, and{" "}
-            <b>grow</b>. BitHacks aims to foster creativity and{" "}
-            <b>push your imagination beyond its limits</b>.
-          </p>
+      {/* <AboutCard /> */}
+      {/* <div className="relative">
+        <div className="h-[65vh] w=[40vw] md:h-[68vh] bg-[#DFBAFF] flex items-center">
+          <div className="bg-white h-[65vh]">
+            <p className="font-rethink text-[1.5rem] md:text-4xl md:leading-[3rem] md:pr-[10rem] lg:pl-[44vw] md:pl-[20vw] pt-8 text-right max-md:px-10 absolute">
+              As UCI's first Embedded Systems hackathon, BitHacks is focused on
+              providing a space for students to <b>create</b>, <b>explore</b>,
+              and <b>grow</b>. BitHacks aims to foster creativity and{" "}
+              <b>push your imagination beyond its limits</b>.
+            </p>
+          </div>
         </div>
-        <div
+      </div> */}
+      {/* <div
           id="about"
           className="w-0 h-0 
         border-l-[98vw] border-l-transparent
         border-t-[75px] border-t-[#DFBAFF]
         border-r-[0] border-r-transparent
         right-0 flex absolute"
-        ></div>
-      </div>
+        ></div> */}
+      {/* </div> */}
 
-      <div className="font-rethink pl-10 md:pl-[10rem] h-[60vh] w-fit flex flex-col justify-center">
-        <div>
-          <p className="">April 11-13 • Doheny Beach Student Center</p>
-          <p className="text-4xl md:text-5xl">36 Hours</p>
-          <p id="prizes" className="text-4xl md:text-5xl pb-4"></p>
-        </div>
-        {/* <motion.button
+      <div
+        style={{
+          backgroundImage: "url('car-graphic.svg')",
+          backgroundRepeat: "no-repeat",
+          backgroundSize: "cover",
+          backgroundPosition: "top",
+        }}
+        className="h-screen overflow-hidden w-[100vw]"
+      >
+        <div className="font-rethink pl-10 md:pl-[10rem] h-[60vh] w-fit flex flex-col justify-center">
+          <div>
+            <p className="">April 11-13 • Doheny Beach Student Center</p>
+            <p className="text-4xl md:text-5xl">36 Hours</p>
+            <p id="prizes" className="text-4xl md:text-5xl pb-4"></p>
+          </div>
+          {/* <motion.button
           whileHover={{ scale: 1.2, backgroundColor: "#724CF1" }}
           whileTap={{ scale: 0.9, backgroundColor: "#724CF1" }}
           className="bg-violet-500 rounded-full text-xl py-2 px-24 md:text-sm md:px-4 md:py-1.5 w-fit"
         >
           Sponsor
         </motion.button> */}
+        </div>
       </div>
     </>
   );
