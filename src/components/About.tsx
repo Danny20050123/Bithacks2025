70a27778e8371652e6e025a890e1bfab5109c4d2
Julia Nguyen <juliatnguyen05@gmail.com>
2025-03-08 22:25:35 -0800

    add mobile resposiveness about section

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -41,9 +41,9 @@ const About = () => {
 
   return (
     <>
-      {/* <AboutCard /> */}
+      {/* Original About Section Layout */}
       {/* <div className="relative">
-        <div className="h-[65vh] w=[40vw] md:h-[68vh] bg-[#DFBAFF] flex items-center">
+        <div className="h-[65vh] w=[40vw] pt-[8rem] md:h-[68vh] bg-[#DFBAFF] flex items-center">
           <div className="bg-white h-[65vh]">
             <p className="font-rethink text-[1.5rem] md:text-4xl md:leading-[3rem] md:pr-[10rem] lg:pl-[44vw] md:pl-[20vw] pt-8 text-right max-md:px-10 absolute">
               As UCI's first Embedded Systems hackathon, BitHacks is focused on
@@ -53,15 +53,15 @@ const About = () => {
             </p>
           </div>
         </div>
-      </div> */}
-      {/* <div
-          id="about"
-          className="w-0 h-0 
+      </div>
+      <div
+        id="about"
+        className="w-0 h-0 
         border-l-[98vw] border-l-transparent
         border-t-[75px] border-t-[#DFBAFF]
         border-r-[0] border-r-transparent
         right-0 flex absolute"
-        ></div> */}
+      ></div> */}
       {/* </div> */}
 
       <div
@@ -71,23 +71,29 @@ const About = () => {
           backgroundSize: "cover",
           backgroundPosition: "bottom left 80%",
         }}
-        className="h-screen overflow-hidden right-5 w-100"
+        className="h-screen flow-hidden right-5 w-100"
       >
-        <div className="relative font-rethink gap-16 pl-10 md:px-[4rem] h-[60vh] w-fit flex flex-row items-center">
-          <AboutCard />
-          <div className="bg-[#6c81e6]/65 w-[32rem] rounded-lg">
-            <p className="md:text-xl">
-              April 11-13 • Doheny Beach Student Center
-            </p>
-            <p className="text-4xl md:text-5xl">36 Hours</p>
-            <p id="prizes" className="h-[10rem] text-4xl md:text-5xl pb-4"></p>
-            {/* <motion.button
+        <div className="relative font-rethink gap-16 m-auto md:m-0 md:pl-10 md:px-[4rem] h-[60vh] w-fit flex flex-row items-center">
+          <div>
+            <AboutCard />
+            <div className="bg-[#6c81e6]/65 mt-8 p-8 w-[20rem] md:w-[32rem] rounded-lg">
+              <p className="md:text-xl">
+                April 11-13 • Doheny Beach Student Center
+              </p>
+              <p className="text-4xl md:text-5xl">36 Hours</p>
+              <p
+                id="prizes"
+                className="h-[10rem] text-4xl md:text-5xl pb-4"
+              ></p>
+              {/* Sponsor Button */}
+              {/* <motion.button
           whileHover={{ scale: 1.2, backgroundColor: "#724CF1" }}
           whileTap={{ scale: 0.9, backgroundColor: "#724CF1" }}
           className="bg-violet-500 rounded-full text-xl py-2 px-24 md:text-sm md:px-4 md:py-1.5 w-fit"
         >
           Sponsor
         </motion.button> */}
+            </div>
           </div>
         </div>
       </div>
--- src/components/AboutCard.tsx
+++ src/components/AboutCard.tsx
@@ -3,16 +3,20 @@ import Image from "next/image";
 const AboutCard = () => {
   return (
     <>
-      <div className="bg-[#DFBAFF] w-fit pt-12 px-6 pb-6 flex flex-col rounded-lg">
-        {/* <div className="bg-white h-[60%] w-[50%]"> */}
-        {/* <div className="bg-white h-4 w-4"></div> */}
-        <p className="font-rethink bg-white h-fit rounded-md w-[20rem] md:w-[50vw] p-8 text-2xl">
+      <div className="bg-[#DFBAFF] w-fit p-6 gap-2 flex flex-col rounded-lg">
+        <Image
+          src="/x.svg"
+          width={30}
+          height={30}
+          alt="x icon"
+          className="bg-white rounded-md justify-end"
+        />
+        <p className="font-rethink bg-white h-fit rounded-md w-[65vw] md:w-[50vw] p-8 text-lg md:text-2xl">
           As UCI's first Embedded Systems hackathon, BitHacks is focused on
           providing a space for students to <b>create</b>, <b>explore</b>, and{" "}
           <b>grow</b>. BitHacks aims to foster creativity and{" "}
           <b>push your imagination beyond its limits</b>.
         </p>
-        {/* </div> */}
       </div>
     </>
   );
